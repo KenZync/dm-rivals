f3da97447b35a1cf94b7bcfff75ba2af471a8ad5
KenZ <kenzync@pm.me>
2022-12-03 21:36:47 +0700

    create folder if not exist

--- server/lib/getRanking.js
+++ server/lib/getRanking.js
@@ -1,11 +1,15 @@
 import fs from "fs";
 export const getRanking = async () => {
   const data = await $fetch(`https://dpjam.net/ranking`);
+  const dir = "./data"
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
   fs.writeFileSync("data/ranking.html", data, (err) => {
     if (err) {
       console.error(err);
       return;
     }
   });
-  return data;
+  return;
 };
