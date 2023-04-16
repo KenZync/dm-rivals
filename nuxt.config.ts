911cab0ed982e31ee4c2c03f70a1266cdcdccb95
Ken Phatipatanawong <kenzync@pm.me>
2023-04-16 13:07:06 +0700

    test ads

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss',],
-    
-})
+  modules: [
+    "@nuxtjs/tailwindcss",
+    [
+      "@nuxtjs/google-adsense",
+      {
+        id: "ca-pub-9393835297385779",
+      },
+    ],
+  ],
+});
